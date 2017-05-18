c02f6fc4977912c97a214ee4d911438042351010
cherry <cherry1012@mail.com>
2017-05-18 16:05:51 +0800

    ==修改脚本

--- boss/V1.0.1.0331__I_pay_api_provider.sql
+++ boss/V1.0.1.0331__I_pay_api_provider.sql
@@ -30,12 +30,12 @@ INSERT INTO "pay_api_provider" ("id", "channel_code", "remarks", "jar_url", "api
   WHERE NOT EXISTS (select channel_code from pay_api_provider where channel_code = 'tianchuang_zfb');
 
 --http://a.bjdpay.com:8209
---http://3rd.pay.api.com/xunjietong-pay
+--http://3rd.pay.api.com:8209/xunjietong-pay
 INSERT INTO "pay_api_provider" ("id", "channel_code", "remarks", "jar_url", "api_class", "jar_version", "ext_json")
-  SELECT '127', 'xunjietong_wx', '迅捷通（微信）', 'file:/data/impl-jars/pay/pay-xunjietong.jar', 'org.soul.pay.impl.XunjietongPayWXApi', '20170515', '{"pro":{"payUrl":"http://a.bjdpay.com:8209/payapi/passivePay","queryOrderUrl":"http://3rd.pay.api.com/xunjietong-pay/payapi/qrcodeQuery"},"test":{"payUrl":"http://a.bjdpay.com:8209/payapi/passivePay","queryOrderUrl":"http://a.bjdpay.com:8209/payapi/qrcodeQuery"}}'
+  SELECT '127', 'xunjietong_wx', '迅捷通（微信）', 'file:/data/impl-jars/pay/pay-xunjietong.jar', 'org.soul.pay.impl.XunjietongPayWXApi', '20170515', '{"pro":{"payUrl":"http://a.bjdpay.com:8209/payapi/passivePay","queryOrderUrl":"http://3rd.pay.api.com:8209/xunjietong-pay/payapi/qrcodeQuery"},"test":{"payUrl":"http://a.bjdpay.com:8209/payapi/passivePay","queryOrderUrl":"http://a.bjdpay.com:8209/payapi/qrcodeQuery"}}'
   WHERE NOT EXISTS (select channel_code from pay_api_provider where channel_code = 'xunjietong_wx');
 
 INSERT INTO "pay_api_provider" ("id", "channel_code", "remarks", "jar_url", "api_class", "jar_version", "ext_json")
-  SELECT '128', 'xunjietong_zfb', '迅捷通（支付宝）', 'file:/data/impl-jars/pay/pay-xunjietong.jar', 'org.soul.pay.impl.XunjietongPayZFBApi', '20170515', '{"pro":{"payUrl":"http://a.bjdpay.com:8209/payapi/passivePay","queryOrderUrl":"http://3rd.pay.api.com/xunjietong-pay/payapi/qrcodeQuery"},"test":{"payUrl":"http://a.bjdpay.com:8209/payapi/passivePay","queryOrderUrl":"http://a.bjdpay.com:8209/payapi/qrcodeQuery"}}'
+  SELECT '128', 'xunjietong_zfb', '迅捷通（支付宝）', 'file:/data/impl-jars/pay/pay-xunjietong.jar', 'org.soul.pay.impl.XunjietongPayZFBApi', '20170515', '{"pro":{"payUrl":"http://a.bjdpay.com:8209/payapi/passivePay","queryOrderUrl":"http://3rd.pay.api.com:8209/xunjietong-pay/payapi/qrcodeQuery"},"test":{"payUrl":"http://a.bjdpay.com:8209/payapi/passivePay","queryOrderUrl":"http://a.bjdpay.com:8209/payapi/qrcodeQuery"}}'
   WHERE NOT EXISTS (select channel_code from pay_api_provider where channel_code = 'xunjietong_zfb');
 
