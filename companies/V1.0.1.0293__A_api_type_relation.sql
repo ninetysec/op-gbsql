47f13b5466b7097ac2ec9e725ad6786cc7d3dd7c
cherry <cherry1012@mail.com>
2017-06-08 15:32:04 +0800

    ==新增api 23 24 脚本

---
+++ companies/V1.0.1.0293__A_api_type_relation.sql
@@ -0,0 +1,10 @@
+-- auto gen by cherry 2017-06-08 15:11:30
+ALTER SEQUENCE api_type_relation_id_seq OWNED BY "api_type_relation"."id";
+
+	CREATE SEQUENCE if not EXISTS "api_type_relation_i18n_id_seq"
+	 INCREMENT 1
+	 MINVALUE 1
+	 MAXVALUE 9223372036854775807
+	 START 100
+	 CACHE 1
+	 OWNED BY "api_type_relation_i18n"."id";
