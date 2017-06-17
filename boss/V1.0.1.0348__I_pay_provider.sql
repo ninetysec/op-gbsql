e7d3fe74a7957cb728eab8231308e06e47bd2c36
cherry <cherry1012@mail.com>
2017-06-17 10:04:08 +0800

    ＝＝新增宝立付

--- boss/V1.0.1.0348__I_pay_provider.sql
+++ boss/V1.0.1.0348__I_pay_provider.sql
@@ -18,3 +18,19 @@ INSERT INTO "pay_api_provider" ("channel_code", "remarks", "jar_url", "api_class
   SELECT 'baolifu_zfb', '宝立付支付（支付宝）', 'file:/data/impl-jars/pay/pay-baolifu.jar', 'org.soul.pay.impl.BaolifuPayZFBApi', '20170603', '{"pro":{"payUrl":"http://119.23.249.156:8080/pp_server/pay","queryOrderUrl":"http://3rd.pay.api.com/baolifu-pay/pp_server/pay"},"test":{"payUrl":"http://119.23.249.156:8080/pp_server/pay","queryOrderUrl":"http://119.23.249.156:8080/pp_server/pay"}}'
 
   WHERE NOT EXISTS (select channel_code from pay_api_provider where channel_code = 'baolifu_zfb');
+
+  --https://gw.169.cc
+--http://3rd.pay.pai.com/duobao-pay
+INSERT INTO "pay_api_provider" ("channel_code", "remarks", "jar_url", "api_class", "jar_version", "ext_json")
+  SELECT 'duobaopay_wy', '多宝支付（网银）', 'file:/data/impl-jars/pay/pay-duobao.jar', 'org.soul.pay.impl.DuobaoPayWYApi', '20170613', '{"pro":{"payUrl":"https://gw.169.cc/interface/AutoBank/index.aspx","queryOrderUrl":"http://3rd.pay.pai.com/duobao-pay/interface/search.aspx"},"test":{"payUrl":"https://gw.169.cc/interface/AutoBank/index.aspx","queryOrderUrl":"https://gw.169.cc/interface/search.aspx"}}'
+  WHERE NOT EXISTS (select channel_code from pay_api_provider where channel_code = 'duobaopay_wy');
+
+
+INSERT INTO "pay_api_provider" ("channel_code", "remarks", "jar_url", "api_class", "jar_version", "ext_json")
+  SELECT 'duobaopay_wx', '多宝支付（微信）', 'file:/data/impl-jars/pay/pay-duobao.jar', 'org.soul.pay.impl.DuobaoPayWXApi', '20170613', '{"pro":{"payUrl":"https://gw.169.cc/interface/AutoBank/index.aspx","queryOrderUrl":"http://3rd.pay.pai.com/duobao-pay/interface/search.aspx"},"test":{"payUrl":"https://gw.169.cc/interface/AutoBank/index.aspx","queryOrderUrl":"https://gw.169.cc/interface/search.aspx"}}'
+  WHERE NOT EXISTS (select channel_code from pay_api_provider where channel_code = 'duobaopay_wx');
+
+
+INSERT INTO "pay_api_provider" ("channel_code", "remarks", "jar_url", "api_class", "jar_version", "ext_json")
+  SELECT 'duobaopay_zfb', '多宝支付（支付宝）', 'file:/data/impl-jars/pay/pay-duobao.jar', 'org.soul.pay.impl.DuobaoPayZFBApi', '20170613', '{"pro":{"payUrl":"https://gw.169.cc/interface/AutoBank/index.aspx","queryOrderUrl":"http://3rd.pay.pai.com/duobao-pay/interface/search.aspx"},"test":{"payUrl":"https://gw.169.cc/interface/AutoBank/index.aspx","queryOrderUrl":"https://gw.169.cc/interface/search.aspx"}}'
+  WHERE NOT EXISTS (select channel_code from pay_api_provider where channel_code = 'duobaopay_zfb');
--- companies/V1.0.1.0306__I_bank.sql
+++ companies/V1.0.1.0306__I_bank.sql
@@ -18,3 +18,21 @@ INSERT INTO "bank" ("bank_name", "bank_icon", "bank_district", "type", "bank_sho
 
   WHERE not EXISTS(SELECT id FROM bank where bank_name='baolifu_zfb');
 
+INSERT INTO "bank" ("bank_name", "bank_icon", "bank_district", "type", "bank_short_name", "bank_icon_simplify", "bank_short_name2", "is_use", "order_num", "pay_type", "website")
+
+  SELECT 'duobaopay_wy', NULL, 'CN', '3', '多宝支付-网银支付', NULL, '多宝支付', 't', '041', '1', NULL
+
+  WHERE not EXISTS(SELECT id FROM bank where bank_name='duobaopay_wy');
+
+INSERT INTO "bank" ("bank_name", "bank_icon", "bank_district", "type", "bank_short_name", "bank_icon_simplify", "bank_short_name2", "is_use", "order_num", "pay_type", "website")
+
+  SELECT 'duobaopay_wx', NULL, 'CN', '3', '多宝支付-微信支付', NULL, '多宝支付', 't', '042', '2', NULL
+
+  WHERE not EXISTS(SELECT id FROM bank where bank_name='duobaopay_wx');
+
+INSERT INTO "bank" ("bank_name", "bank_icon", "bank_district", "type", "bank_short_name", "bank_icon_simplify", "bank_short_name2", "is_use", "order_num", "pay_type", "website")
+
+  SELECT 'duobaopay_zfb', NULL, 'CN', '3', '多宝支付-支付宝', NULL, '多宝支付', 't', '043', '3', NULL
+
+  WHERE not EXISTS(SELECT id FROM bank where bank_name='duobaopay_zfb');
+
