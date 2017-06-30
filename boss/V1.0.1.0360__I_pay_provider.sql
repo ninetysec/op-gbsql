61fe4539766c199995c22e957fc1ad7f70be68db
cherry <cherry1012@mail.com>
2017-06-30 11:59:31 +0800

    ＝=新增轻易付 add by cherry

--- boss/V1.0.1.0360__I_pay_provider.sql
+++ boss/V1.0.1.0360__I_pay_provider.sql
@@ -3,7 +3,7 @@
 --http://3rd.pay.api.com/qingyifu
 INSERT INTO "pay_api_provider" ("channel_code", "remarks", "jar_url", "api_class", "jar_version", "ext_json")
   SELECT 'qingyifu_wx', '轻易付（微信）', 'file:/data/impl-jars/pay/pay-qingyifu.jar', 'org.soul.pay.impl.QingyifuPayWXApi', '20170629',
-'{"pro":{"payUrl":"hhttp://3rd.pay.api.com/qingyifu/api/pay.action","queryOrderUrl":"http://3rd.pay.api.com/qingyifu/api/queryPayResult.action"},"test":{"payUrl":"http://47.90.116.117:90/api/pay.action","queryOrderUrl":"http://47.90.116.117:90/api/queryPayResult.action"}}'
+'{"pro":{"payUrl":"http://3rd.pay.api.com/qingyifu/api/pay.action","queryOrderUrl":"http://3rd.pay.api.com/qingyifu/api/queryPayResult.action"},"test":{"payUrl":"http://47.90.116.117:90/api/pay.action","queryOrderUrl":"http://47.90.116.117:90/api/queryPayResult.action"}}'
   WHERE NOT EXISTS (select channel_code from pay_api_provider where channel_code = 'qingyifu_wx');
 
 INSERT INTO "pay_api_provider" ("channel_code", "remarks", "jar_url", "api_class", "jar_version", "ext_json")
