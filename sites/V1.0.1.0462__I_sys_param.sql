edb611663db0fea8cf0c7fda708dfd591d69e282
cherry <cherry1012@mail.com>
2017-07-03 10:11:12 +0800

    ＝=新增脚本

---
+++ sites/V1.0.1.0462__I_sys_param.sql
@@ -0,0 +1,9 @@
+-- auto gen by cherry 2017-06-30 16:18:40
+INSERT INTO"sys_param" ("module", "param_type", "param_code", "param_value", "default_value", "order_num", "remark", "parent_code", "active", "site_id")
+SELECT 'setting', 'withdraw_type', 'is_cash', 'true', 'true', NULL, '是否用现金打款', NULL, 't', NULL
+WHERE not EXISTS(SELECT id FROM sys_param WHERE module='setting' and param_type='withdraw_type' and param_code='is_cash');
+
+
+INSERT INTO"sys_param" ("module", "param_type", "param_code", "param_value", "default_value", "order_num", "remark", "parent_code", "active", "site_id")
+SELECT 'setting', 'withdraw_type', 'is_bitcoin', 'true', 'false', NULL, '是否用比特币打款', NULL, 't', NULL
+WHERE not EXISTS(SELECT id FROM sys_param WHERE module='setting' and param_type='withdraw_type' and param_code='is_bitcoin');
---
+++ sites/V1.0.1.0464__C_v_player_deposit.sql
@@ -0,0 +1,57 @@
+-- auto gen by cherry 2017-07-03 09:58:46
+DROP VIEW IF EXISTS v_player_deposit;
+CREATE OR REPLACE VIEW "v_player_deposit" AS
+ SELECT pr.id,
+    pr.player_id,
+    su.username,
+    COALESCE(pt.rank_id, up.rank_id) AS rank_id,
+    pr.create_time,
+    pr.recharge_type,
+    pr.is_first_recharge,
+    pr.payer_bank,
+    pr.payer_bankcard,
+    pr.bank_order,
+    pr.recharge_address,
+    pr.pay_account_id,
+    pa.bank_code,
+    pa.full_name,
+    pr.counter_fee,
+    su.default_currency,
+    pr.recharge_amount,
+    pr.recharge_total_amount,
+    pr.check_status,
+    pr.recharge_status,
+    pr.check_user_id,
+    pr.check_username,
+    pr.check_time,
+    pr.payer_name,
+    pr.transaction_no,
+    pa.custom_bank_name,
+    pa.account,
+    pr.recharge_type_parent,
+    pr.check_remark,
+    pr.failure_title,
+    pa.status AS pay_account_status,
+    pa.deposit_count,
+    up.recharge_count,
+    pr.ip_deposit,
+    pr.ip_dict_code,
+    pr.pay_url,
+    pt.origin,
+    pa.pay_name,
+    ra.rank_name,
+    ra.risk_marker,
+    pa.account_type,
+    pa.channel_json,
+    pr.favorable_total_amount,
+    pr.return_time,
+    pr.bit_amount,
+		pr.player_transaction_id
+   FROM (((((player_recharge pr
+     LEFT JOIN sys_user su ON ((pr.player_id = su.id)))
+     LEFT JOIN user_player up ON ((pr.player_id = up.id)))
+     LEFT JOIN pay_account pa ON ((pr.pay_account_id = pa.id)))
+     LEFT JOIN player_transaction pt ON (((pr.transaction_no)::text = (pt.transaction_no)::text)))
+     LEFT JOIN player_rank ra ON ((ra.id = COALESCE(pt.rank_id, up.rank_id))));
+
+COMMENT ON VIEW "v_player_deposit" IS 'Fei - 玩家存款列表视图';
