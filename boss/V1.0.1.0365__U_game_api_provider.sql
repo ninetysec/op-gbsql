15be814dabba62553bd948ccc0e585a51fa0d2b8
cherry <cherry1012@mail.com>
2017-07-04 10:53:27 +0800

    ＝=修改新霸 add by zain

---
+++ boss/V1.0.1.0365__U_game_api_provider.sql
@@ -0,0 +1,3 @@
+-- auto gen by cherry 2017-07-04 10:52:33
+UPDATE "game_api_provider" set "id"='25', "abbr_name"='spade', "full_name"='', "api_url"='http://3rd.game.api.com/spade-api/api/', "remarks"='正式环境:http://spadegaming.ampinplayopt0matrix.com', "jar_url"='file:/data/impl-jars/api/api-spade.jar', "api_class"='so.wwb.gamebox.service.gameapi.impl.SpadeGameApi', "jar_version"='20170615066678', "ext_json"='{"merchantCode":"DAWOO","search-minute":"10","validateTokenExpire":60,"gameUrl":"http://lobby.bigmoose88.com/dawoo/auth/","pageIndex":0}', "default_timezone"='+0', "support_agent"='f' WHERE "id"='25';
+
