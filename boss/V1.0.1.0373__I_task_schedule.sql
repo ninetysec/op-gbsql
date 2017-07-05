ac88cfd5d115b176ad1dd111eb39ca312d17d31b
cherry <cherry1012@mail.com>
2017-07-05 21:37:53 +0800

    ＝=新增任务

---
+++ boss/V1.0.1.0373__I_task_schedule.sql
@@ -0,0 +1,4 @@
+-- auto gen by cherry 2017-07-05 21:37:19
+ INSERT INTO "task_schedule" ("job_name", "alias_name", "job_group", "job_class", "job_method", "is_local", "status", "cronexpression", "is_sync", "description", "create_time", "update_time", "job_code", "is_system", "is_dynamic", "job_method_arg", "job_method_arg_class", "belong_to_idc", "scheduler")
+SELECT 'apiId-25-SPADE-新增下单记录', NULL, NULL, 'so.wwb.gamebox.service.company.PlayerGameOrderFetchJob', 'execute', 't', '2', '0 0/4 * * * ?', 't', 'api任务', '2017-05-24 02:13:48.132917', NULL, 'api-25-I', 'f', 'f', '25', 'java.lang.Integer', 'A', 'scheduler4Api'
+WHERE not EXISTS(SELECT id FROM task_schedule WHERE job_code='api-25-I');
