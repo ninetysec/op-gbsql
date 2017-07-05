7269da5cded5ae0629aca33bb60ddd30b0bbbdfd
water <water@wwb.so>
2017-07-05 20:17:01 +0800

    SQL: 禁用老的两个站点任务,及处理qrtz*,线上修改为手动执行

--- boss/V1.0.1.0372__D_truncate_all_qrtz_prefix_tabs.sql
+++ boss/V1.0.1.0372__D_truncate_all_qrtz_prefix_tabs.sql
@@ -1,9 +1,9 @@
 -- auto gen by Water 2017-07-05 17:12:07
 
 --调度器: 删除调度器临时表,删除后需要boss重新初始化之
-TRUNCATE table qrtz_locks;
-TRUNCATE table qrtz_scheduler_state;
-TRUNCATE table qrtz_paused_trigger_grps;
-TRUNCATE table qrtz_calendars;
-TRUNCATE table qrtz_job_details CASCADE ;
-TRUNCATE table qrtz_fired_triggers;
+--TRUNCATE table qrtz_locks;
+--TRUNCATE table qrtz_scheduler_state;
+--TRUNCATE table qrtz_paused_trigger_grps;
+--TRUNCATE table qrtz_calendars;
+--TRUNCATE table qrtz_job_details CASCADE ;
+--TRUNCATE table qrtz_fired_triggers;
