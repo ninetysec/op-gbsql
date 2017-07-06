c8b9b2b2b09d4740e67a0515a51b71cbc12c3000
cherry <cherry1012@mail.com>
2017-07-06 09:36:54 +0800

    ＝=修改默认值

--- sites/V1.0.1.0462__I_sys_param.sql
+++ sites/V1.0.1.0462__I_sys_param.sql
@@ -5,5 +5,5 @@ WHERE not EXISTS(SELECT id FROM sys_param WHERE module='setting' and param_type=
 
 
 INSERT INTO"sys_param" ("module", "param_type", "param_code", "param_value", "default_value", "order_num", "remark", "parent_code", "active", "site_id")
-SELECT 'setting', 'withdraw_type', 'is_bitcoin', 'true', 'false', NULL, '是否用比特币打款', NULL, 't', NULL
+SELECT 'setting', 'withdraw_type', 'is_bitcoin', 'false', 'false', NULL, '是否用比特币打款', NULL, 't', NULL
 WHERE not EXISTS(SELECT id FROM sys_param WHERE module='setting' and param_type='withdraw_type' and param_code='is_bitcoin');
