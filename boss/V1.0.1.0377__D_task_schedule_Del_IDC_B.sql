b0fa1c12ebc48fbbdc97d7cbb41eeb8bf0a307f9
water <water@wwb.so>
2017-07-08 11:59:53 +0800

    SQL: 清理调度任务,只保留一个导出任务

---
+++ boss/V1.0.1.0377__D_task_schedule_Del_IDC_B.sql
@@ -0,0 +1,11 @@
+-- auto gen by Water 2017-07-08 11:46:12
+
+DELETE from task_schedule t where t.belong_to_idc = 'B';
+
+--任务调度:只保留导出功能
+insert into task_schedule(job_name, alias_name, job_group, job_class, job_method, is_local, status, cronexpression, is_sync, description, create_time, update_time, job_code, is_system, is_dynamic, job_method_arg, job_method_arg_class, belong_to_idc, scheduler)
+SELECT job_name, alias_name, job_group, job_class, job_method, is_local, status, cronexpression, is_sync, description, create_time, update_time, job_code, is_system, is_dynamic, job_method_arg, job_method_arg_class, 'B', scheduler
+from task_schedule
+where job_code = 'once-003'
+and not exists( SELECT id from task_schedule  where job_code = 'once-003' and belong_to_idc = 'B');
+
