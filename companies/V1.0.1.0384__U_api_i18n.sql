32e7b1451eee37d9d0d3e1cb8de287b6c68384ff
cherry <cherry1012@mail.com>
2017-08-07 17:54:34 +0800

    ==修改api日语版本 add by leo

---
+++ companies/V1.0.1.0384__U_api_i18n.sql
@@ -0,0 +1,40 @@
+-- auto gen by cherry 2017-08-07 17:41:00
+UPDATE "api_i18n" SET "name"='BSG' WHERE ("api_id"='20' and "locale"='ja_JP');
+
+UPDATE "site_api_i18n" SET "name"='BSG' WHERE ("api_id"='20' and "local"='ja_JP');
+
+UPDATE "api_type_relation_i18n" SET "name"='BSG' WHERE ("api_id"='20' and "local"='ja_JP');
+
+UPDATE "site_api_type_relation_i18n" SET "name"='BSG' WHERE ("api_id"='20' and "local"='ja_JP');
+
+UPDATE "api_i18n" SET "name"='SG電子ゲーム?' WHERE ("api_id"='25' and "locale"='ja_JP');
+
+UPDATE "site_api_i18n" SET "name"='SG電子ゲーム?' WHERE ("api_id"='25' and "local"='ja_JP');
+
+UPDATE "api_type_relation_i18n" SET "name"='SG電子ゲーム?' WHERE ("api_id"='25' and "local"='ja_JP');
+
+UPDATE "site_api_type_relation_i18n" SET "name"='SG電子ゲーム?' WHERE ("api_id"='25' and "local"='ja_JP');
+
+UPDATE "api_i18n" SET "name"='PNG電子ゲーム?' WHERE ("api_id"='26' and "locale"='ja_JP');
+
+UPDATE "site_api_i18n" SET "name"='PNG電子ゲーム?' WHERE ("api_id"='26' and "local"='ja_JP');
+
+UPDATE "api_type_relation_i18n" SET "name"='PNG電子ゲーム?' WHERE ("api_id"='26' and "local"='ja_JP');
+
+UPDATE "site_api_type_relation_i18n" SET "name"='PNG電子ゲーム?' WHERE ("api_id"='26' and "local"='ja_JP');
+
+UPDATE "api_i18n" SET "name"='DT電子ゲーム?' WHERE ("api_id"='27' and "locale"='ja_JP');
+
+UPDATE "site_api_i18n" SET "name"='DT電子ゲーム?' WHERE ("api_id"='27' and "local"='ja_JP');
+
+UPDATE "api_type_relation_i18n" SET "name"='DT電子ゲーム?' WHERE ("api_id"='27' and "local"='ja_JP');
+
+UPDATE "site_api_type_relation_i18n" SET "name"='DT電子ゲーム?' WHERE ("api_id"='27' and "local"='ja_JP');
+
+UPDATE "api_i18n" SET "name"='GGフィッシング' WHERE ("api_id"='28' and "locale"='ja_JP');
+
+UPDATE "site_api_i18n" SET "name"='GGフィッシング' WHERE ("api_id"='28' and "local"='ja_JP');
+
+UPDATE "api_type_relation_i18n" SET "name"='GGフィッシング' WHERE ("api_id"='28' and "local"='ja_JP');
+
+UPDATE "site_api_type_relation_i18n" SET "name"='GGフィッシング' WHERE ("api_id"='28' and "local"='ja_JP');
