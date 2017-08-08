13a755b37662ef1071fd86f9ec36be7f481bdb72
water <water@wwb.so>
2017-08-08 11:21:52 +0800

    站点用户:增加开站时间(为v1053发布做准备缓存)

---
+++ companies/V1.0.1.0385__A_v_site_user_OpenTime.sql
@@ -0,0 +1,20 @@
+-- auto gen by longer 2017-08-08 11:20:44
+
+--站点用户:增加开站时间(为v1053发布做准备缓存)
+CREATE or REPLACE VIEW v_sys_site_user AS SELECT site.id,
+                                            site.name AS site_name,
+                                            usr.id AS sys_user_id,
+                                            site.status,
+                                            usr.username,
+                                            usr.subsys_code,
+                                            usr.site_id AS center_id,
+                                            site.parent_id AS site_parent_id,
+                                            site.main_language AS site_locale,
+                                            site.timezone,
+                                            ds.idc,
+                                            site.opening_time
+                                          FROM sys_site site
+                                            LEFT JOIN sys_datasource ds
+                                              on (site.id = ds.id),
+                                            sys_user usr
+                                          WHERE (site.sys_user_id = usr.id);
