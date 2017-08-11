cc125a865300f3c0f5b8c3b8e080eecf2a823caf
cherry <cherry1012@mail.com>
2017-08-11 16:24:00 +0800

    ==新增彩票派彩任务 add by younger

---
+++ boss/V1.0.1.0401__I_task_schedule.sql
@@ -0,0 +1,4 @@
+-- auto gen by cherry 2017-08-11 16:22:52
+INSERT INTO "task_schedule" ("job_name", "alias_name", "job_group", "job_class", "job_method", "is_local", "status", "cronexpression", "is_sync", "description", "create_time", "update_time", "job_code", "is_system", "is_dynamic", "job_method_arg", "job_method_arg_class", "belong_to_idc", "scheduler")
+SELECT '彩票派彩任务', NULL, NULL, 'so.wwb.gamebox.service.company.LotteryResultOpenJob', 'execute', 't', '1', '0 0/3 * * * ?', 't', '彩票派彩任务', '2017-08-11 06:43:33.906', NULL, 'lottery_result_payout', 'f', 'f', '5', 'java.lang.Integer', 'B', 'scheduler4Lottery'
+WHERE NOT EXISTS (SELECT job_code from task_schedule where job_code='lottery_result_payout');
