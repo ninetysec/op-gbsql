9601029c9dcc9a9c10a16dbee451258bf9707734
cherry <cherry1012@mail.com>
2017-08-12 16:16:48 +0800

    ==修复数据

--- boss/V1.0.1.0399__D_task_schedule_DEL_jackpot.sql
+++ boss/V1.0.1.0399__D_task_schedule_DEL_jackpot.sql
@@ -4,5 +4,5 @@
 DELETE from task_schedule t where t.job_code = 'site_jackpot_for_api';
 
 --任务调度下线:和站长彩池n贡献金对账(由于每个月都跑不完,将由DBA手动跑)
-DELETE from 有task_schedule t where t.job_code = 'site_jackpot_for_master';
+DELETE from task_schedule t where t.job_code = 'site_jackpot_for_master';
 
