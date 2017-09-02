5254571d0ab7fa67db381d912aa1c9268dbc7604
cherry <cherry1012@mail.com>
2017-09-02 15:01:17 +0800

    ==修改脚本

--- companies/V1.0.1.0399__A_lottery_payout_record.sql
+++ companies/V1.0.1.0399__A_lottery_payout_record.sql
@@ -3,4 +3,4 @@ select redo_sqls($$
 alter table lottery_payout_record add column payout_hash  varchar(50);
  ALTER TABLE "lottery_payout_record" ADD CONSTRAINT "unique_payout_hash" UNIQUE ("payout_hash");
   $$);
- COMMENT ON COLUMN lottery_gather_conf.conf_type IS '开奖hash，避免同时开奖';
+ COMMENT ON COLUMN lottery_gather_conf.payout_hash IS '开奖hash，避免同时开奖';
