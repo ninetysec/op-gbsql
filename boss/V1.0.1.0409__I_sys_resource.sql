c7614a31dda4672d6f1148440c719a3febac6e09
cherry <cherry1012@mail.com>
2017-09-05 17:50:20 +0800

    添加彩票菜单

--- boss/V1.0.1.0409__I_sys_resource.sql
+++ boss/V1.0.1.0409__I_sys_resource.sql
@@ -2,3 +2,5 @@
 INSERT INTO sys_resource (id,name, url, remark, parent_id, structure, sort_num, subsys_code, permission, resource_type, icon, privilege, built_in, status)
 SELECT 70601,'彩票开奖异常日志手动派彩', 'lotteryResult/delete.html', '彩票开奖异常日志手动派彩', '706', '', '1', 'boss', 'lottery:hand_payout', '2', '', 'f', 't', 't'
  WHERE NOT EXISTS (SELECT id FROM sys_resource  WHERE  id='70601') ;
+
+  INSERT INTO sys_resource (id,name, url, remark, parent_id, structure, sort_num, subsys_code, permission, resource_type, icon, privilege, built_in, status)         SELECT 70404,'手动删除开奖结果记录', 'lotteryResult/delete.html', '手动删除开奖结果记录', '704', '', '4', 'boss', 'lottery:opencode_delete', '2', '', 'f', 't', 't'  WHERE NOT EXISTS (SELECT id FROM sys_resource  WHERE  id=70404) ;
