0303e6d0cc0e78768fe7adbe83237125518eb512
cherry <cherry1012@mail.com>
2017-09-28 16:48:25 +0800

    ==注释脚本 add by younger

--- sites/V1.0.1.0535__C_sys_resource.sql
+++ sites/V1.0.1.0535__C_sys_resource.sql
@@ -1,4 +1,5 @@
 -- auto gen by cherry 2017-09-26 10:14:50
+/*
 INSERT INTO "sys_resource" ("id", "name", "url", "remark", "parent_id", "structure", "sort_num", "subsys_code", "permission", "resource_type", "icon", "built_in", "privilege", "status")
 	SELECT '40105', '设置前端展示', 'operation/activity/changeDisplayStatus.html', '设置前端展示功能权限', '401', NULL, NULL, 'mcenter', 'operate:activity_showfront', '2', NULL, 't', 'f', 't'
-		 WHERE NOT EXISTS (select id from sys_resource t where t.id = 40105);
+		 WHERE NOT EXISTS (select id from sys_resource t where t.id = 40105);*/
