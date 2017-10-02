f347cc153dc647d36dc6ddd739e01e299ef8894c
cherry <cherry1012@mail.com>
2017-10-02 18:00:12 +0800

    ==添加大额监控菜单

--- boss/V1.0.1.0420__I_sys_resource.sql
+++ boss/V1.0.1.0420__I_sys_resource.sql
@@ -3,4 +3,4 @@ INSERT INTO "sys_resource" ("id", "name", "url", "remark", "parent_id", "structu
 SELECT '30619', '转账上限修改', '', '转账上限修改', '306', '', '17', 'boss', 'siteManage:transfer', '2', '', 'f', 't', 't' WHERE NOT EXISTS(SELECT ID FROM sys_resource WHERE permission='siteManage:transfer');
 
 INSERT INTO "gb-boss"."sys_resource" ("id", "name", "url", "remark", "parent_id", "structure", "sort_num", "subsys_code", "permission", "resource_type", "icon", "privilege", "built_in", "status")
-SELECT '803', '大额监控', 'largeTransactionMonitor/list.html', '大额监控', '8', '', '3', 'boss', 'credit:account', '1', '', 'f', 't', 't' WHERE NOT EXISTS(SELECT ID FROM sys_resource WHERE permission='credit:account');
+SELECT '803', '大额监控', 'largeTransactionMonitor/list.html', '大额监控', '8', '', '3', 'boss', 'credit:largeMonitor', '1', '', 'f', 't', 't' WHERE NOT EXISTS(SELECT ID FROM sys_resource WHERE id='803');
