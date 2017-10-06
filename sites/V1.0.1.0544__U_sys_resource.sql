13260c0c79151093e086d9895a0314b18c952535
linsen <linsen@me.so>
2017-10-06 19:18:22 +0800

    ==更新菜单@by younger

---
+++ sites/V1.0.1.0544__U_sys_resource.sql
@@ -0,0 +1,3 @@
+-- auto gen by george 2017-10-06 19:15:06
+update sys_resource set url = 'vUserAgentManage/list.html' where id=22 and subsys_code='mcenterTopAgent';
+update sys_resource set url = 'home/index.html' where id=21 and subsys_code='mcenterTopAgent';
