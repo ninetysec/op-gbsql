74ba61cce9c6b2f30649f5e6ab00d692f7f7b87f
marz <zhuzhidu784>
2017-10-09 16:38:53 +0800

    1.彩票盈利报表统计函数 2.彩票盈利报表创建语句 3.彩票盈利报表

---
+++ boss/V1.0.1.0427__U_sys_resource.sql
@@ -0,0 +1,2 @@
+-- auto gen by marz 2017-10-09 16:36:52
+UPDATE "sys_resource" SET "id"='709', "name"='报表', "url"='lotteryReportProfit/reportlist.html', "remark"='彩票管理-报表', "parent_id"='7', "structure"=NULL, "sort_num"='4', "subsys_code"='boss', "permission"='lottery:report', "resource_type"='1', "icon"='icon-xiane', "privilege"='f', "built_in"='t', "status"='t' WHERE ("id"='709');
