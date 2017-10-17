4fd6c1c6367236b2b6183062fc7e15b01022b36b
cherry <cherry1012@mail.com>
2017-10-17 17:11:16 +0800

    ==数字货币

--- companies/V1.0.1.0441__I_site_job.sql
+++ companies/V1.0.1.0441__I_site_job.sql
@@ -8,14 +8,14 @@ AND dict_code='BTC');
 INSERT INTO "site_job" ("id", "prefix_job_id", "sub_job_code", "sub_job_name",
 "job_class", "job_method", "job_type", "status", "job_time_type", "job_time_unit",
 "fixed_month", "fixed_day", "fixed_hour", "fixed_minutes", "fixed_second", "period_value")
-SELECT '17', NULL, 'site_job_017', '数字货币获取历史记录任务', 'so.wwb.gamebox.service.master.DigiccyHistoryJob',
+SELECT '104', NULL, 'site_job_104', '数字货币获取历史记录任务', 'so.wwb.gamebox.service.master.DigiccyHistoryJob',
 'siteJob', '2', '1', '2', '2', NULL, NULL, NULL, NULL, NULL, '6'
-WHERE not EXISTS(SELECT id FROM site_job where sub_job_code='site_job_017');
+WHERE not EXISTS(SELECT id FROM site_job where sub_job_code='site_job_104');
 
-DELETE FROM site_job where id=18;
+
 INSERT INTO "site_job" ("id", "prefix_job_id", "sub_job_code", "sub_job_name",
 "job_class", "job_method", "job_type", "status", "job_time_type", "job_time_unit",
 "fixed_month", "fixed_day", "fixed_hour", "fixed_minutes", "fixed_second", "period_value")
-SELECT '18', NULL, 'site_job_018', '数字货币超时订单任务', 'so.wwb.gamebox.service.master.DigiccyRechargeJob',
+SELECT '105', NULL, 'site_job_105', '数字货币超时订单任务', 'so.wwb.gamebox.service.master.DigiccyRechargeJob',
 'siteJob', '2', '1', '2', '2', NULL, NULL, NULL, NULL, NULL, '3'
-WHERE not EXISTS(SELECT id FROM site_job where sub_job_code='site_job_018');
+WHERE not EXISTS(SELECT id FROM site_job where sub_job_code='site_job_105');
