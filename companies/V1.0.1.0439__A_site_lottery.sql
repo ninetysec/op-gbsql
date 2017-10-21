96cfa13d2a84c46cfe11ed3cb4640b7f89492248
marz <zhuzhidu784>
2017-10-21 11:28:48 +0800

    修改lottery与site_lottery增加字段时候的方式

--- companies/V1.0.1.0439__A_site_lottery.sql
+++ companies/V1.0.1.0439__A_site_lottery.sql
@@ -1,3 +1,5 @@
 -- auto gen by marz 2017-10-09 19:34:29
+ select redo_sqls($$
 ALTER TABLE site_lottery ADD genre int4 DEFAULT 3;
+$$);
 COMMENT ON COLUMN "site_lottery"."genre" IS '类型(1.全部2.官方玩法3.双面玩法)';
--- companies/V1.0.1.0440__A_lottery.sql
+++ companies/V1.0.1.0440__A_lottery.sql
@@ -1,3 +1,5 @@
 -- auto gen by marz 2017-10-09 19:36:54
+ select redo_sqls($$
 ALTER TABLE lottery ADD genre int4 DEFAULT 3;
+$$);
 COMMENT ON COLUMN "lottery"."genre" IS '类型(1.全部2.官方玩法3.双面玩法)';
