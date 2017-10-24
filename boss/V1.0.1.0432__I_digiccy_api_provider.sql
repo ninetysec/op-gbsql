ab0d4ae2423c97d853d9d022c274874bd9f5166a
cherry <cherry1012@mail.com>
2017-10-24 14:48:28 +0800

    ==新增数字货币接口 digiccy_api_provider

--- boss/V1.0.1.0432__I_digiccy_api_provider.sql
+++ boss/V1.0.1.0432__I_digiccy_api_provider.sql
@@ -1,6 +1,6 @@
 -- auto gen by cherry 2017-10-24 14:11:13
 INSERT INTO "digiccy_api_provider" ("code", "public_url", "private_url", "provider_class")
-SELECT 'draglet', 'http://3rd.game.api.com/digiccy-api/draglet/gateway/public/', 'http://3rd.game.api.com/digiccy-api/draglet/gateway/private/', 'so.wwb.gamebox.service.master.digiccy.DragletClient'
+SELECT 'draglet', 'http://3rd.game.api.com/digiccy-api/gateway/public/', 'http://3rd.game.api.com/digiccy-api/gateway/private/', 'so.wwb.gamebox.service.master.digiccy.DragletClient'
 WHERE not EXISTS (SELECT id FROM digiccy_api_provider where code='draglet');
 
 
