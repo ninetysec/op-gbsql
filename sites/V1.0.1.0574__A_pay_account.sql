3d840b10fd3bc7619a48c6b2a76d02839b75c79d
cherry <cherry1012@mail.com>
2017-10-26 20:52:05 +0800

    ==收款账号添加字段

---
+++ sites/V1.0.1.0574__A_pay_account.sql
@@ -0,0 +1,8 @@
+-- auto gen by cherry 2017-10-26 20:51:30
+ select redo_sqls($$
+   ALTER TABLE pay_account add COLUMN alias_name varchar(32);
+	ALTER TABLE pay_account ADD COLUMN support_atm_counter BOOLEAN;
+  $$);
+
+COMMENT on COLUMN pay_account.alias_name is '别名';
+COMMENT on COLUMN pay_account.support_atm_counter is '是否在柜台';
