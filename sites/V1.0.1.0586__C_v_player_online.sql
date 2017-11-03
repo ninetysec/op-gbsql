b6eab664668c361558876ceab2c17fc32d078e32
linsen <linsen@me.so>
2017-11-03 15:51:00 +0800

    ==修改在线玩家视图时间查询@by leo

--- sites/V1.0.1.0586__C_v_player_online.sql
+++ sites/V1.0.1.0586__C_v_player_online.sql
@@ -14,6 +14,7 @@ CREATE OR REPLACE VIEW "v_player_online" AS
     s.last_login_ip_dict_code,
     s.total_online_time,
     string_agg(((pgl.game_id)::character varying)::text, ','::text) AS gameids,
+    string_agg(((pgl.api_id)::character varying)::text, ','::text) AS apiids,
     s.session_key,
     u.wallet_balance,
     u.freezing_funds_balance,
@@ -23,5 +24,5 @@ CREATE OR REPLACE VIEW "v_player_online" AS
    FROM ((user_player u
      JOIN sys_user s ON ((s.id = u.id)))
      LEFT JOIN player_game_log pgl ON (((pgl.user_id = s.id) AND ((pgl.session_key)::text = (s.session_key)::text))))
-  WHERE s.session_key IS NOT NULL AND (s.login_time > s.last_logout_time OR s.last_logout_time IS NULL) AND s.last_active_time > (now() - '00:30:00'::interval)
+  WHERE ((s.session_key IS NOT NULL) AND ((s.login_time > s.last_logout_time) OR (s.last_logout_time IS NULL)) AND (s.last_active_time > (now() - '00:30:00'::interval)))
   GROUP BY s.id, s.username, s.real_name, s.login_time, s.login_ip, s.login_ip_dict_code, s.last_active_time, s.use_line, s.last_login_time, s.last_login_ip, s.last_login_ip_dict_code, s.total_online_time, s.session_key, u.wallet_balance, u.freezing_funds_balance, u.rank_id, u.channel_terminal, s.terminal;
