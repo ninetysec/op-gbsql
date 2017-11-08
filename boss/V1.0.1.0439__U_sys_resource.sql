452117178cb5085d311b3b5d894ed89b3013100e
marz <zhuzhidu784>
2017-11-08 09:42:26 +0800

    修改派彩与重结权限脚本

--- boss/V1.0.1.0439__U_sys_resource.sql
+++ boss/V1.0.1.0439__U_sys_resource.sql
@@ -2,3 +2,5 @@
 UPDATE sys_resource SET  "name"='调盘', "remark"='调盘' WHERE "id"='70405';
 UPDATE sys_resource SET  "name"='撤单', "remark"='撤单' WHERE "id"='71001';
 UPDATE sys_resource SET  "name"='撤销', "remark"='撤销' WHERE "id"='71002';
+UPDATE sys_resource set url='lotterySysTool/heavy.html' WHERE id=71005;
+UPDATE sys_resource set url='lotterySysTool/payout.html' WHERE id=71004;
