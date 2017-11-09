c4043c91fb5268dfd4755aaa3070944c9e797ceb
marz <zhuzhidu784>
2017-11-09 19:18:40 +0800

    数据字典重接,撤单,撤销脚本回退

--- companies/V1.0.1.0457__I_sys_dict.sql
+++ companies/V1.0.1.0457__I_sys_dict.sql
@@ -2,15 +2,3 @@
 INSERT INTO sys_dict ( "module", "dict_type", "dict_code", "order_num", "remark", "parent_code", "active")
 SELECT 'lottery', 'transaction_type', '5', '5', '返点', NULL, 't'
 where '5' not in(SELECT dict_code FROM sys_dict WHERE module='lottery' and dict_type='transaction_type');
-
-INSERT INTO sys_dict ( "module", "dict_type", "dict_code", "order_num", "remark", "parent_code", "active")
-SELECT 'lottery', 'transaction_type', '6', '6', '重结', NULL, 't'
-where '6' not in(SELECT dict_code FROM sys_dict WHERE module='lottery' and dict_type='transaction_type');
-
-INSERT INTO sys_dict ( "module", "dict_type", "dict_code", "order_num", "remark", "parent_code", "active")
-SELECT 'lottery', 'transaction_type', '7', '7', '待结撤单', NULL, 't'
-where '7' not in(SELECT dict_code FROM sys_dict WHERE module='lottery' and dict_type='transaction_type');
-
-INSERT INTO sys_dict ( "module", "dict_type", "dict_code", "order_num", "remark", "parent_code", "active")
-SELECT 'lottery', 'transaction_type', '8', '8', '已结撤销', NULL, 't'
-where '8' not in(SELECT dict_code FROM sys_dict WHERE module='lottery' and dict_type='transaction_type');
