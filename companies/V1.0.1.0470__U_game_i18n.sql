4d820197d5f9d6cbcbf3ee73c5e5f3e13c0424bc
marz <zhuzhidu784>
2017-11-15 09:37:46 +0800

    修改game_i18n中的极速PK10

---
+++ companies/V1.0.1.0470__U_game_i18n.sql
@@ -0,0 +1,9 @@
+-- auto gen by marz 2017-11-15 09:25:04
+UPDATE game_i18n set name = '极速PK10' where  game_id=220041 and locale='zh_CN';
+UPDATE game_i18n set name = '极速PK10' where  game_id=220042 and locale='zh_CN';
+UPDATE game_i18n set name = '极速PK10' where  game_id=220041 and locale='zh_TW';
+UPDATE game_i18n set name = '极速PK10' where  game_id=220042 and locale='zh_TW';
+UPDATE game_i18n set name = '极速PK10' where  game_id=220041 and locale='en_US';
+UPDATE game_i18n set name = '极速PK10' where  game_id=220042 and locale='en_US';
+UPDATE game_i18n set name = '极速PK10' where  game_id=220041 and locale='ja_JP';
+UPDATE game_i18n set name = '极速PK10' where  game_id=220042 and locale='ja_JP';
