7efe0db1f203f16ad527a958224e5c1bc67bf9c8
linsen <linsen@me.so>
2017-11-22 15:26:06 +0800

    ==CREDIT视图 @by kobe

---
+++ companies/V1.0.1.0482__C_v_sys_credit.sql
@@ -0,0 +1,34 @@
+-- auto gen by george 2017-11-22 15:24:28
+DROP VIEW IF EXISTS v_sys_credit;
+
+CREATE OR REPLACE VIEW "v_sys_credit" AS
+ SELECT ss.id,
+    su.owner_id center_id,
+    ss.name site_name,
+    ss.sys_user_id master_id,
+    su.username master_name,
+    (COALESCE(ss.max_profit, 0) + COALESCE(ss.credit_line,0)) as max_profit_limit,
+    ss.has_use_profit,
+    ss.max_profit,
+    CAST(((COALESCE(ss.has_use_profit,0)/(COALESCE(ss.max_profit, 0.01) + COALESCE(ss.credit_line,0)))*100) as decimal(38, 2)) AS percent,
+
+
+    (select sum(pay_amount) from credit_record cr where cr.status='2' AND pay_type = '1' AND cr.create_time>= (SELECT date_trunc(
+		'month',
+		timezone (timezone, now())
+	) - REPLACE (timezone, 'GMT', '') :: INTERVAL FROM sys_site WHERE id=ss.id) AND cr.create_time<(SELECT date_trunc(
+		'month',
+		timezone (timezone, now() + '1mon')
+	) - REPLACE (timezone, 'GMT', '') :: INTERVAL FROM sys_site WHERE id=ss.id) AND cr.site_id=ss.id) AS all_recharge,
+
+
+
+    ss.credit_line,
+		ss.profit_time,
+    (CASE WHEN COALESCE(ss.max_profit, 0) + COALESCE(ss.credit_line,0) > COALESCE(ss.has_use_profit,0) THEN 0 ELSE 1 END) AS status
+   FROM (sys_site ss
+     LEFT JOIN sys_user su ON ((ss.sys_user_id = su.id)))
+  WHERE ss.id>0
+  ORDER BY percent DESC;
+
+COMMENT ON VIEW "v_sys_credit" IS 'CREDIT视图 add by kobe';
