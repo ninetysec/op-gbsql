45e7ee3bc938cbc18a013186e5ea690ab30286e0
linsen <linsen@me.so>
2017-11-28 21:10:43 +0800

    ==修改玩家存取款审核查看权限脚本@by linsen

--- sites/V1.0.1.0610__I_sys_resource.sql
+++ sites/V1.0.1.0610__I_sys_resource.sql
@@ -4,7 +4,7 @@ SELECT '30102', '查看', '', '公司入款审核-查看', '301', '', NULL, 'mce
 WHERE NOT EXISTS(SELECT ID FROM sys_resource WHERE ID='30102');
 
 INSERT INTO "sys_resource" ("id", "name", "url", "remark", "parent_id", "structure", "sort_num", "subsys_code", "permission", "resource_type", "icon", "built_in", "privilege", "status")
-SELECT '30304', '查看', '', '玩家取款审核-查看', '303', '', NULL, 'mcenter', 'fund:playerwithdraw', '2', '', 't', 'f', 't'
+SELECT '30304', '查看', 'fund/withdraw/withdrawAuditView.html', '玩家取款审核-查看', '303', '', NULL, 'mcenter', 'fund:playerwithdraw', '2', '', 't', 'f', 't'
 WHERE NOT EXISTS(SELECT ID FROM sys_resource WHERE ID='30304');
 
 INSERT INTO "sys_resource" ("id", "name", "url", "remark", "parent_id", "structure", "sort_num", "subsys_code", "permission", "resource_type", "icon", "built_in", "privilege", "status")
