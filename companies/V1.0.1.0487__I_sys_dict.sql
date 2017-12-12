cc48d31e0443cae5edcad0bcc5e092fde23c6842
linsen <linsen@me.so>
2017-12-12 20:20:19 +0800

    @by leo

--- companies/V1.0.1.0487__I_sys_dict.sql
+++ companies/V1.0.1.0487__I_sys_dict.sql
@@ -3,10 +3,10 @@ INSERT INTO "sys_dict" ("module", "dict_type", "dict_code", "order_num", "remark
 'fund', 'recharge_type', 'jdpay_scan', '15', '线上支付：京东钱包支付', 'online_deposit', 't'
 WHERE NOT EXISTS(SELECT ID FROM sys_dict WHERE dict_type='recharge_type' AND dict_code='jdpay_scan');
 
-INSERT INTO "sys_dict" ("module", "dict_type", "dict_code", "order_num", "remark", "parent_code", "active") SELECT
-'fund', 'recharge_type', 'union_pay_scan', '16', '线上支付：银联扫码支付', 'online_deposit', 't'
-WHERE NOT EXISTS(SELECT ID FROM sys_dict WHERE dict_type='recharge_type' AND dict_code='union_pay_scan');
-
-INSERT INTO "sys_dict" ("module", "dict_type", "dict_code", "order_num", "remark", "parent_code", "active") SELECT
-'fund', 'recharge_type', 'qqwallet_scan', '13', '线上支付：QQ钱包支付', 'online_deposit', 't'
-WHERE NOT EXISTS(SELECT ID FROM sys_dict WHERE dict_type='recharge_type' AND dict_code='qqwallet_scan');
+-- INSERT INTO "sys_dict" ("module", "dict_type", "dict_code", "order_num", "remark", "parent_code", "active") SELECT
+-- 'fund', 'recharge_type', 'union_pay_scan', '16', '线上支付：银联扫码支付', 'online_deposit', 't'
+-- WHERE NOT EXISTS(SELECT ID FROM sys_dict WHERE dict_type='recharge_type' AND dict_code='union_pay_scan');
+--
+-- INSERT INTO "sys_dict" ("module", "dict_type", "dict_code", "order_num", "remark", "parent_code", "active") SELECT
+-- 'fund', 'recharge_type', 'qqwallet_scan', '13', '线上支付：QQ钱包支付', 'online_deposit', 't'
+-- WHERE NOT EXISTS(SELECT ID FROM sys_dict WHERE dict_type='recharge_type' AND dict_code='qqwallet_scan');
