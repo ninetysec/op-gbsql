6ae9744e22785799111574ead4cbfc82c94242b2
linsen <linsen@me.so>
2017-12-13 10:15:59 +0800

    修改脚本@leo

--- sites/V1.0.1.0631__U_ctt_float_pic_item.sql
+++ sites/V1.0.1.0631__U_ctt_float_pic_item.sql
@@ -1,2 +1,2 @@
 -- auto gen by george 2017-12-13 10:06:35
-UPDATE ctt_float_pic_item SET img_link_protocol='http://' WHERE img_link_type='link' AND img_link_protocol = '';
+UPDATE ctt_float_pic_item SET img_link_protocol='http://' WHERE img_link_type='link' AND img_link_protocol IS NULL;
