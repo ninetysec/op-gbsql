cded75d2aab871173fa28850d0ec4d733b14368b
linsen <linsen@me.so>
2017-12-21 15:15:43 +0800

    修改脚本

--- boss/V1.0.1.0470__U_sys_param.sql
+++ boss/V1.0.1.0470__U_sys_param.sql
@@ -1,4 +1,1 @@
 -- auto gen by george 2017-12-18 19:41:21
-UPDATE "sys_param" SET "param_code"='paycname', "param_value"='/pay/cname.html', "default_value"='/pay/cname.html', "remark"='支付cname' WHERE ("id"='72');
-
-UPDATE "sys_param" SET "param_code"='indexcname', "param_value"='/index/cname.html', "default_value"='/index/cname.html', "remark"='主页cname' WHERE ("id"='6');
