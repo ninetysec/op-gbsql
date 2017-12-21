3e25e887d9cb422d90d7835c52cbfb61713199c4
linsen <linsen@me.so>
2017-12-21 16:42:07 +0800

    支付cname @by back

--- boss/V1.0.1.0475__I_sys_param.sql
+++ boss/V1.0.1.0475__I_sys_param.sql
@@ -8,5 +8,5 @@ SELECT 'content', 'domain_type', 'paycname', '/pay/cname.html', '/pay/cname.html
 WHERE NOT EXISTS(SELECT param_code, module,param_type FROM sys_param WHERE param_code = 'paycname' AND module = 'content' AND param_type='domain_type');
 
 INSERT INTO "sys_param" ( "module", "param_type", "param_code", "param_value", "default_value", "order_num", "remark", "parent_code", "active", "site_id")
-SELECT 'content', 'domain_type', 'indexcname', '/index/cname.html', '/index/cname.html', '2', '主页name', '', 't', '0'
+SELECT 'content', 'domain_type', 'indexcname', '/index/cname.html', '/index/cname.html', '2', '主页cname', '', 't', '0'
 WHERE NOT EXISTS(SELECT param_code, module,param_type FROM sys_param WHERE param_code = 'indexcname' AND module = 'content' AND param_type='domain_type');
