5470e72e2aeb1dfea7e93ecbc83f189c663bd7b1
linsen <linsen@me.so>
2018-01-07 15:50:08 +0800

    支付日志 @by lin

--- boss/V1.0.1.0484__A_pay_log.sql
+++ boss/V1.0.1.0484__A_pay_log.sql
@@ -4,9 +4,11 @@ select  redo_sqls ($$
     ALTER TABLE pay_log ADD COLUMN merchant_code varchar(32);
     ALTER TABLE pay_log ADD COLUMN terminal varchar(2) ;
     ALTER TABLE pay_log ADD COLUMN error_log varchar(255) ;
+    ALTER TABLE pay_log ADD COLUMN provider varchar(32);
 $$);
 
 COMMENT ON COLUMN pay_log.channel_code is '支付渠道';
 COMMENT ON COLUMN pay_log.merchant_code is '商户号';
 COMMENT ON COLUMN pay_log.terminal is '终端';
 COMMENT ON COLUMN pay_log.error_log is '错误信息';
+COMMENT ON COLUMN pay_log.provider is '提供商';
