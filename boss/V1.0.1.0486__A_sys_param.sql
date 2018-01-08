7f30cad6170cf341abf85eb349cf840a073611d1
cherry <cherry1012@mail.com>
2018-01-08 20:37:28 +0800

    ==补充缺失字段

---
+++ boss/V1.0.1.0486__A_sys_param.sql
@@ -0,0 +1,6 @@
+-- auto gen by cherry 2018-01-08 20:36:44
+select redo_sqls($$
+    ALTER TABLE sys_param add COLUMN "operate" int4;
+$$);
+
+COMMENT on COLUMN sys_param.operate is '操作权限，boss:0, 站长:1  全部支持:2';
