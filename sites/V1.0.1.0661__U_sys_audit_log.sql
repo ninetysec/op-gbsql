150bea84cba39ca7d94a6bb70e3589c402a31f5c
linsen <linsen@me.so>
2018-01-11 19:54:57 +0800

    @by leo

---
+++ sites/V1.0.1.0661__U_sys_audit_log.sql
@@ -0,0 +1,2 @@
+-- auto gen by george 2018-01-11 19:54:23
+UPDATE "sys_audit_log" SET  "module_type"='43' WHERE operate_url='/mcenter/payAccount/saveOnLine.html' AND module_name='pay' AND module_obj='pay';
