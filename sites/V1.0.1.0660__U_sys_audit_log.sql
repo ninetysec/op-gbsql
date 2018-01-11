7b2106266bcaeaa38cf3c45943823720d5e8c72a
linsen <linsen@me.so>
2018-01-11 19:52:55 +0800

    修改脚本@by leo

--- sites/V1.0.1.0660__U_sys_audit_log.sql
+++ sites/V1.0.1.0660__U_sys_audit_log.sql
@@ -1,2 +1,2 @@
 -- auto gen by george 2018-01-11 19:45:39
-UPDATE "sys_audit_log" SET  "module_type"='43' WHERE operate_url='/mcenter/payAccount/saveOnLine.html' AND module_name='pay' AND module_obj='pay';
+-- UPDATE "sys_audit_log" SET  "module_type"='43' WHERE operate_url='/mcenter/payAccount/saveOnLine.html' AND module_name='pay' AND module_obj='pay';
