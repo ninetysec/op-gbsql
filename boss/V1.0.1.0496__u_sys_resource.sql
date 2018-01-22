67cff358cbef675745b8e890414217a7d24b7964
linsen <linsen@me.so>
2018-01-22 15:28:29 +0800

    域名审核@by steffan

---
+++ boss/V1.0.1.0496__u_sys_resource.sql
@@ -0,0 +1,4 @@
+-- auto gen by george 2018-01-22 15:17:31
+UPDATE "sys_resource" SET "url" = 'vDomainCheck/list.html' WHERE  ("id" = '201');
+
+UPDATE "sys_resource" SET "url" = 'vDomainCheck/list.html' WHERE  ("id" = '20104');
