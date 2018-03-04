3277b212f38e91eb5cc4fa00a8246ba3ca21a0d3
linsen <linsen@me.so>
2018-03-04 19:05:46 +0800

    cnd开关 @by mical

---
+++ sites/V1.0.1.0694__I_sys_param.sql
@@ -0,0 +1,5 @@
+-- auto gen by linsen 2018-03-04 19:03:57
+-- author : mical
+INSERT INTO "sys_param" ("id", "module", "param_type", "param_code", "param_value", "default_value", "order_num", "remark", "parent_code", "active", "site_id", "is_switch", "operate")
+select '1000', 'setting', 'system_settings', 'cdn_switch', 'true', '', NULL, 'cnd开关', '', 't', NULL, 'true', '0'
+where not EXISTS(SELECT ID FROM sys_param WHERE module ='setting' AND  param_type = 'system_settings' AND param_code='cdn_switch');
