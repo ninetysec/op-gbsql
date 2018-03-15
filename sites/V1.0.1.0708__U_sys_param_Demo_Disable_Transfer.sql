22a417a5522cf9e598b1a8b394ad462b9d71c3d1
water <water@wwb.so>
2018-03-15 19:51:29 +0800

    站点参数设置：可以设置“开关站点转帐”&& 演示站(默认禁止转帐)参数

--- sites/V1.0.1.0708__U_sys_param_Demo_Disable_Transfer.sql
+++ sites/V1.0.1.0708__U_sys_param_Demo_Disable_Transfer.sql
@@ -4,4 +4,4 @@ update sys_param set operate = 0
 where module = 'setting' and param_type = 'fund' and param_code = 'disable_transfer';
 
 update sys_param set operate = 0,remark = '演示站(默认禁止转帐)'
-where module = 'setting' and param_type = 'fund' and param_code = 'demo'
+where module = 'setting' and param_type = 'fund' and param_code = 'demo';
