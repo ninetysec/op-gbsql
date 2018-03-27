293fff21a5bbeb4aa501b237cc8485eac5f538fe
linsen <linsen@me.so>
2018-03-27 16:10:56 +0800

    取消輸入安全碼 by back

---
+++ sites/V1.0.1.0715__U_sys_resource.sql
@@ -0,0 +1,8 @@
+-- auto gen by linsen 2018-03-27 16:10:16
+--站長中心-前端展示，參數設置-取消輸入安全碼 by back
+
+UPDATE "sys_resource" SET "privilege"='f' WHERE ("id"='70104');
+
+UPDATE "sys_resource" SET "privilege"='f' WHERE ("id"='70105');
+
+UPDATE "sys_resource" SET "privilege"='f' WHERE ("id"='7010501');
