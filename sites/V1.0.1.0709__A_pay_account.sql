b1ef37b9a1aaa8469bf6cb46afa6c631faca42cd
linsen <linsen@me.so>
2018-04-02 20:54:02 +0800

    修改脚本

--- sites/V1.0.1.0709__A_pay_account.sql
+++ sites/V1.0.1.0709__A_pay_account.sql
@@ -1,9 +1,8 @@
 -- auto gen by linsen 2018-03-16 18:11:19
 -- pay_account添加自定义账号信息、自定义账号提示字段 by gavin
  select redo_sqls($$
-    ALTER TABLE "pay_account"
-    ADD COLUMN "account_information" VARCHAR(30),
-		ADD COLUMN "account_prompt" VARCHAR(30);
+    ALTER TABLE "pay_account" ADD COLUMN "account_information" VARCHAR(30);
+		ALTER TABLE "pay_account" ADD COLUMN "account_prompt" VARCHAR(30);
 $$);
 
 COMMENT ON COLUMN "pay_account"."account_information" IS '自定义账号信息';
