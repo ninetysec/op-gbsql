1eaab7e6087fe7ac6366d545ef927ed59ad60143
linsen <linsen@me.so>
2018-04-09 10:10:55 +0800

    总控api资金管理菜单功能权限  by pack

---
+++ boss/V1.0.1.0543__I_sys_resource.sql
@@ -0,0 +1,10 @@
+-- auto gen by linsen 2018-04-09 10:07:28
+-- 总控api资金管理菜单功能权限  by pack
+
+INSERT INTO "sys_resource" ("id", "name", "url", "remark", "parent_id", "structure", "sort_num", "subsys_code", "permission", "resource_type", "icon", "privilege", "built_in", "status")
+SELECT '914', 'API资金管理', 'VPlayerApi/list.html', 'API资金管理', '9', '', '4', 'boss', 'api:vplayerApi', '1', '', 'f', 't', 't'
+WHERE not EXISTS(SELECT id FROM sys_resource where id='914');
+
+INSERT INTO "sys_resource" ("id", "name", "url", "remark", "parent_id", "structure", "sort_num", "subsys_code", "permission", "resource_type", "icon", "privilege", "built_in", "status")
+SELECT '91401', 'API回收资金', '', 'API回收资金', '914', '', '1', 'boss', 'api:vplayerapi_recovery', '2', '', 'f', 't', 't'
+WHERE not EXISTS(SELECT id FROM sys_resource where id='91401');
