f7ec57b2c6624107c85c88e58149bdf641e42f19
linsen <linsen@me.so>
2018-04-09 10:26:14 +0800

    总控api资金管理菜单功能权限  by pack

--- boss/V1.0.1.0543__I_sys_resource.sql
+++ boss/V1.0.1.0543__I_sys_resource.sql
@@ -6,5 +6,5 @@ SELECT '914', 'API资金管理', 'VPlayerApi/list.html', 'API资金管理', '9',
 WHERE not EXISTS(SELECT id FROM sys_resource where id='914');
 
 INSERT INTO "sys_resource" ("id", "name", "url", "remark", "parent_id", "structure", "sort_num", "subsys_code", "permission", "resource_type", "icon", "privilege", "built_in", "status")
-SELECT '91401', 'API回收资金', '', 'API回收资金', '914', '', '1', 'boss', 'api:vplayerapi_recovery', '2', '', 'f', 't', 't'
+SELECT '91401', 'API回收资金', 'VPlayerApi/withdrawBalance.html', 'API回收资金', '914', '', '1', 'boss', 'api:vplayerapi_recovery', '2', '', 'f', 't', 't'
 WHERE not EXISTS(SELECT id FROM sys_resource where id='91401');
