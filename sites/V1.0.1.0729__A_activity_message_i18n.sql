0442c233a63fd85f8d50f474ace011ccaa1b94ca
linsen <linsen@me.so>
2018-04-17 14:49:01 +0800

    活动相关新加字段移动到2017版本

---
+++ sites/V1.0.1.0729__A_activity_message_i18n.sql
@@ -0,0 +1,6 @@
+-- auto gen by linsen 2018-04-09 11:52:05
+-- activity_message_i18n添加字段 by kobe
+select redo_sqls($$
+  ALTER TABLE activity_message_i18n ADD COLUMN activity_terminal_type VARCHAR(8);
+$$);
+COMMENT ON COLUMN activity_message_i18n.activity_terminal_type IS '活动终端类型';
