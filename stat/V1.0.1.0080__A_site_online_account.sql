91d3b1c212102e32b54851c59a9df11ea8b36b8a
linsen <linsen@me.so>
2018-05-04 14:27:42 +0800

    site_online_account添加字段 by kobe

--- stat/V1.0.1.0080__A_site_online_account.sql
+++ stat/V1.0.1.0080__A_site_online_account.sql
@@ -3,14 +3,14 @@
 select redo_sqls($$
   ALTER TABLE site_online_account ADD COLUMN player_guest INT4;
   ALTER TABLE site_online_account ADD COLUMN real_guest INT4;
-  ALTER TABLE site_online_account ADD COLUMN player_online_from_pc INT4;
-  ALTER TABLE site_online_account ADD COLUMN player_online_from_h5 INT4;
-  ALTER TABLE site_online_account ADD COLUMN player_online_from_ios INT4;
-  ALTER TABLE site_online_account ADD COLUMN player_online_from_Android INT4;
+  ALTER TABLE site_online_account ADD COLUMN player_pc INT4;
+  ALTER TABLE site_online_account ADD COLUMN player_h5 INT4;
+  ALTER TABLE site_online_account ADD COLUMN player_ios INT4;
+  ALTER TABLE site_online_account ADD COLUMN player_Android INT4;
 $$);
 COMMENT ON COLUMN site_online_account.player_guest IS '游客数量';
 COMMENT ON COLUMN site_online_account.real_guest IS '去重游客数量';
-COMMENT ON COLUMN site_online_account.player_online_from_pc IS '来自pc端玩家';
-COMMENT ON COLUMN site_online_account.player_online_from_h5 IS '来自h5玩家';
-COMMENT ON COLUMN site_online_account.player_online_from_ios IS '来自ios玩家';
-COMMENT ON COLUMN site_online_account.player_online_from_Android IS '来自android玩家';
+COMMENT ON COLUMN site_online_account.player_pc IS '来自pc端玩家';
+COMMENT ON COLUMN site_online_account.player_h5 IS '来自h5玩家';
+COMMENT ON COLUMN site_online_account.player_ios IS '来自ios玩家';
+COMMENT ON COLUMN site_online_account.player_Android IS '来自android玩家';
