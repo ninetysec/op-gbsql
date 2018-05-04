894e77fe2f02d0a90757b6201b2701252bd6c325
linsen <linsen@me.so>
2018-05-04 14:20:02 +0800

    site_online_account添加字段 by kobe

---
+++ stat/V1.0.1.0080__A_site_online_account.sql
@@ -0,0 +1,16 @@
+-- auto gen by linsen 2018-05-04 14:18:28
+-- site_online_account添加字段 by kobe
+select redo_sqls($$
+  ALTER TABLE site_online_account ADD COLUMN player_guest INT4;
+  ALTER TABLE site_online_account ADD COLUMN real_guest INT4;
+  ALTER TABLE site_online_account ADD COLUMN player_online_from_pc INT4;
+  ALTER TABLE site_online_account ADD COLUMN player_online_from_h5 INT4;
+  ALTER TABLE site_online_account ADD COLUMN player_online_from_ios INT4;
+  ALTER TABLE site_online_account ADD COLUMN player_online_from_Android INT4;
+$$);
+COMMENT ON COLUMN site_online_account.player_guest IS '游客数量';
+COMMENT ON COLUMN site_online_account.real_guest IS '去重游客数量';
+COMMENT ON COLUMN site_online_account.player_online_from_pc IS '来自pc端玩家';
+COMMENT ON COLUMN site_online_account.player_online_from_h5 IS '来自h5玩家';
+COMMENT ON COLUMN site_online_account.player_online_from_ios IS '来自ios玩家';
+COMMENT ON COLUMN site_online_account.player_online_from_Android IS '来自android玩家';
