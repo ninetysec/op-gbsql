0e6cf3d13d5455130c9bdcb51c2c97d0ca6df57e
steffan <steffan@dawoo.com>
2018-05-29 10:48:22 +0800

    add by steffan

--- sites/V1.0.1.0844__U_player_transaction.sql
+++ sites/V1.0.1.0844__U_player_transaction.sql
@@ -12,4 +12,4 @@ UPDATE player_transaction SET origin = CASE origin WHEN 'PC' THEN '1' WHEN 'MOBI
 and create_time <= '2018-02-01' and create_time > '2018-01-01';
 
 UPDATE player_transaction SET origin = CASE origin WHEN 'PC' THEN '1' WHEN 'MOBILE' THEN '2' END WHERE origin IN ( 'PC', 'MOBILE')
-and create_time <= '2017-01-01' and create_time > '2017-12-01';
+and create_time <= '2017-01-01' and create_time > '2017-11-28';
