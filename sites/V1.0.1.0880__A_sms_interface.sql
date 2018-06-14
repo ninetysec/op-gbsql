937e2ec31fbefda69eb5060f39d3168d067657fc
linsen <linsen@me.so>
2018-06-14 14:56:45 +0800

    更新短信接口账号，密码长度限制 by carl

---
+++ sites/V1.0.1.0880__A_sms_interface.sql
@@ -0,0 +1,6 @@
+-- auto gen by linsen 2018-06-14 14:50:10
+-- 更新短信接口账号，密码长度限制 by carl
+SELECT redo_sqls($$
+	ALTER TABLE sms_interface alter COLUMN username type character varying(64);
+	ALTER TABLE sms_interface alter COLUMN password type character varying(64);
+$$);
