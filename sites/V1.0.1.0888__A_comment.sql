d2213def0f52de350aed3384a96bfa5f51bd4744
steffan <steffan@dawoo.com>
2018-06-26 09:56:56 +0800

    终端注释

---
+++ sites/V1.0.1.0888__A_comment.sql
@@ -0,0 +1,4 @@
+-- auto gen by steffan 2018-05-24 11:05:00  add by steffan
+COMMENT ON COLUMN  "sys_user"."terminal" IS '登录终端：1-PC，2-MOBILE,8-h5,12-android,16-ios';
+COMMENT ON COLUMN  "player_transaction"."origin" IS '交易订单来源：1-PC,2-MOBILE,8-h5,12-android,16-ios';
+COMMENT ON COLUMN  "player_game_order"."terminal" IS '注单终端:1-PC 2-MOBILE,8-h5,12-android,16-ios';
