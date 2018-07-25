7e51e8e31219621e18fa40575fc73ecbe69aebd2
linsen <linsen@me.so>
2018-07-25 17:08:04 +0800

    修改返水函数

--- sites/V1.0.1.0924__C_gb_rakeback_player.sql
+++ sites/V1.0.1.0924__C_gb_rakeback_player.sql
@@ -1,5 +1,4 @@
--- auto gen by linsen 2018-07-25 16:13:41
-
+-- auto gen by linsen 2018-07-25 17:07:04
 DROP FUNCTION IF EXISTS gb_rakeback_player(INT, TEXT);
 create or replace function gb_rakeback_player(
   p_bill_id   INT,
@@ -10,6 +9,7 @@ create or replace function gb_rakeback_player(
 --v1.00  2017/01/18  Laser   创建此函数: 返水.玩家返水
 --v1.10  2017/07/01  Laser   增加pending_lssuing字段，以支持返水重结
 --v1.20  2018/06/28  Linsen  添加代理线
+--v1.21  2018/07/25  Laser   修改分组条件保证player_id, agent_id唯一
 */
 DECLARE
 
@@ -23,23 +23,29 @@ BEGIN
     )
     SELECT p_bill_id, ut.id, ut.username, ut.rank_id, ut.rank_name, ut.risk_marker, 'pending_lssuing',
            --agent_id, top_agent_id,
-           ra.agent_id, ra.topagent_id, ra.audit_num, ra.rakeback, ra.rakeback, 0, ra.rakeback, ra.agent_username, ra.topagent_username
+           ra.agent_id, uta.id topagent_id, ra.audit_num, ra.rakeback, ra.rakeback, 0, ra.rakeback, ua.username agent_username, uta.username topagent_username
       FROM (
             SELECT player_id,
 									 agent_id, --v1.20  2018/06/28  Linsen
-									 agent_username,
-									 topagent_id,
-									 topagent_username,
+									 --agent_username,
+									 --topagent_id,
+									 --topagent_username,
                    CASE WHEN SUM(rakeback) > MIN(rakeback_limit) THEN COALESCE(MIN(rakeback_limit),0) ELSE COALESCE(SUM(rakeback),0) END rakeback, --顺序不能改，因为返水上限可能为NULL
                    MIN(audit_num) audit_num,
                    SUM(effective_transaction) effective_transaction
               FROM rakeback_api
              WHERE rakeback_bill_id = p_bill_id
              --v1.20  2018/06/28  Linsen
-             GROUP BY player_id, agent_id, agent_username, topagent_id, topagent_username
-          ) ra,
-          v_sys_user_tier ut
-     WHERE ra.player_id = ut."id"
+             --GROUP BY player_id, agent_id, agent_username, topagent_id, topagent_username
+             --v1.21  2018/07/25
+             GROUP BY player_id, agent_id
+          ) ra
+            LEFT JOIN
+          v_sys_user_tier ut ON ra.player_id = ut.id
+            LEFT JOIN
+          sys_user ua ON ra.agent_id = ua.id
+            LEFT JOIN
+          sys_user uta ON ua.owner_id = uta.id
      ORDER BY ra.rakeback DESC, ut.id, ra.agent_id;
 
   ELSEIF p_settle_flag = 'N' THEN--未出账
@@ -49,23 +55,29 @@ BEGIN
         agent_id, top_agent_id, audit_num, rakeback_total, agent_username, topagent_username
     )
     SELECT p_bill_id, ut.id, ut.username, ut.rank_id, ut.rank_name, ut.risk_marker,
-           ra.agent_id, ra.topagent_id, ra.audit_num, ra.rakeback, ra.agent_username, ra.topagent_username
+           ra.agent_id, uta.id topagent_id, ra.audit_num, ra.rakeback, ua.username agent_username, uta.username topagent_username
       FROM (
             SELECT player_id,
 									 agent_id, --v1.20  2018/06/28  Linsen
-									 agent_username,
-									 topagent_id,
-									 topagent_username,
-                   CASE WHEN SUM(rakeback) > MIN(rakeback_limit) THEN  COALESCE(MIN(rakeback_limit), 0) ELSE SUM(rakeback) END rakeback, --顺序不能改，因为返水上限可能为NULL
+									 --agent_username,
+									 --topagent_id,
+									 --topagent_username,
+                   CASE WHEN SUM(rakeback) > MIN(rakeback_limit) THEN COALESCE(MIN(rakeback_limit),0) ELSE COALESCE(SUM(rakeback),0) END rakeback, --顺序不能改，因为返水上限可能为NULL
                    MIN(audit_num) audit_num,
                    SUM(effective_transaction) effective_transaction
               FROM rakeback_api_nosettled
              WHERE rakeback_bill_nosettled_id = p_bill_id
              --v1.20  2018/06/28  Linsen
-             GROUP BY player_id, agent_id, agent_username, topagent_id, topagent_username
-           ) ra,
-           v_sys_user_tier ut
-     WHERE ra.player_id = ut.id
+             --GROUP BY player_id, agent_id, agent_username, topagent_id, topagent_username
+             --v1.21  2018/07/25
+             GROUP BY player_id, agent_id
+          ) ra
+            LEFT JOIN
+          v_sys_user_tier ut ON ra.player_id = ut.id
+            LEFT JOIN
+          sys_user ua ON ra.agent_id = ua.id
+            LEFT JOIN
+          sys_user uta ON ua.owner_id = uta.id
      ORDER BY ra.rakeback DESC, ut.id;
 
   END IF;
