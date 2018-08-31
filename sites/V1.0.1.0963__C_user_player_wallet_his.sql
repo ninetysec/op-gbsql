83da4af94160bd044d14a4ae5d01c9510f7b7cc9
steffan <steffan@dawoo.com>
2018-08-31 21:04:54 +0800

    修改玩家钱包更新历史记录表 by linsen

--- sites/V1.0.1.0963__C_user_player_wallet_his.sql
+++ sites/V1.0.1.0963__C_user_player_wallet_his.sql
@@ -17,7 +17,7 @@ COMMENT ON COLUMN  "user_player_wallet_history"."create_time" IS '创建时间';
 COMMENT ON COLUMN  "user_player_wallet_history"."remark" IS '备注';
 
 
-DROP TRIGGER "tg_user_player_wallet" ON  "user_player";
+DROP TRIGGER IF EXISTS "tg_user_player_wallet" ON  "user_player";
 CREATE TRIGGER "tg_user_player_wallet" AFTER UPDATE OF "wallet_balance" ON  "user_player"
 FOR EACH ROW
 EXECUTE PROCEDURE process_tg_user_player_wallet();
