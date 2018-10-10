aefcb1db03200dc4d27daa98eafbf4162317afb6
steffan <steffan@dawoo.com>
2018-10-10 11:26:44 +0800

    add by snow -- dba要求不提交git

--- sites/V1.0.1.1001__A_player_game_order.sql
+++
@@ -1,5 +0,0 @@
--- auto gen by steffan 2018-10-10 09:53:09  alter by snow
- select redo_sqls($$
-  alter table player_game_order add column stat_time TIMESTAMP(6);
-  $$);
-COMMENT ON COLUMN player_game_order.stat_time IS '输赢报表统计时间';
